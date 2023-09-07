8a0c741d0952620b969612323f7b99a7dfb349ad
YenHsing <andrew82465@gmail.com>
2023-09-07 18:41:30 -0400

    add debounce to beep_keypad

--- Audio/h_beep_keypad/beep_keypad.c
+++ Audio/h_beep_keypad/beep_keypad.c
@@ -195,6 +195,13 @@ static PT_THREAD (protothread_core_0(struct pt *pt))
     static int i ;
     static uint32_t keypad ;
 
+    
+    // State machine variables for debouncing
+    static enum { NoPressed, MaybePressed, Pressed, MaybeNoPressed } state = NoPressed ;
+    static int possible_key = -1 ;
+    static int confirmed_key = -1 ;
+
+
     while(1) {
 
         gpio_put(LED, !gpio_get(LED)) ;
@@ -222,6 +229,45 @@ static PT_THREAD (protothread_core_0(struct pt *pt))
         }
         // Otherwise, indicate invalid/non-pressed buttons
         else (i=-1) ;
+
+
+        // Debounce the button
+        switch (state) {
+            case NoPressed:
+                if (i != -1) {
+                    state = MaybePressed ;
+                    possible_key = i ;
+                }
+                break ;
+            case MaybePressed:
+                if (i == possible_key) {
+                    state = Pressed ;
+                    confirmed_key = i ;
+                }
+                else {
+                    state = NoPressed ;
+                }
+                break ;
+            case Pressed:
+                if (i != confirmed_key) {
+                    state = MaybeNoPressed ;
+                }
+                break ;
+            case MaybeNoPressed:
+                if (i != confirmed_key) {
+                    state = NoPressed ;
+                    confirmed_key = -1 ;
+                }
+                else {
+                    state = Pressed ;
+                }
+                break ;
+        }
+
+        // Overwrite i with confirmed key,
+        // To avoid changes to original code
+        i = confirmed_key ;
+
 
         if(i != -1) {
             // Set beep state to on
